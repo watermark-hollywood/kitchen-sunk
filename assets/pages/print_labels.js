1ad3d06f68bbda97fecf0d3ac892448f4571ce76
Ryan Miller <Ryan@Ryans-MacBook-Pro.local>
2017-04-07 17:12:15 -0500

    Merge remote-tracking branch 'origin/Ryan' into Ryan
    # Conflicts:
    #	on-hands-count.html
    #	restaurants.html

--- assets/pages/print_labels.js
+++ assets/pages/print_labels.js
@@ -5,12 +5,17 @@ var handleDataTableButtons = function () {
         dom: "Bfrtip",
         pageLength: "50",
         select: true,
+        order: [1, "asc"],
+        columnDefs: [
+            {"orderable": false, "targets": [0]}
+        ],
         buttons: [
             {
                 text: 'Select all',
                 className: "btn-primary btn-trans waves-effect waves-primary w-md m-b-5",
                 action: function () {
                     table.rows().select();
+                    $(".checkbox").prop('checked', true);
                 }
             },
             {
@@ -18,6 +23,7 @@ var handleDataTableButtons = function () {
                 className: "btn-primary btn-trans waves-effect waves-primary w-md m-b-5",
                 action: function () {
                     table.rows().deselect();
+                    $(".checkbox").prop('checked', false);
                 }
             }
         ],
